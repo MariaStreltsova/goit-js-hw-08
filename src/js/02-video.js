c415ee1700cfc5b813e93cff0d1acc68bfc2868b
MariaStreltsova <Mariastreltova@gmail.com>
2022-05-03 21:18:40 +0300

    update 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,13 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+
  const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
 player.on('timeupdate', onPlay);
 player.on('loaded', throttle(updateTime, 1000));
+
 function onPlay() {
  player.getCurrentTime().then(function(seconds) {
     localStorage.setItem(LOCALSTORAGE_KEY, seconds);
